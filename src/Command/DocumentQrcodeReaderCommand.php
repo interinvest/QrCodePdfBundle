7863e27bb8e18a9441c10b3e4fb08fe6a906f588
yassine <ybellaaquoul@inter-invest.fr>
2017-07-18 17:59:58 +0200

    Update 11/07

--- src/Command/DocumentQrcodeReaderCommand.php
+++ src/Command/DocumentQrcodeReaderCommand.php
@@ -19,38 +19,84 @@ class DocumentQrcodeReaderCommand extends ContainerAwareCommand
             ->addOption('option', null, InputOption::VALUE_NONE, 'Option description');
     }
 
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $dir = $this->getContainer()->get('kernel')->getRootDir() . '/../../uploads/';
         $file = $dir .  'NCA-20170705100943-yellow.pdf';
         $im = new \Imagick();
-        $pdf = new \TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
         $im->readImage($file);
         $x = 75;
         $nombrePage = $im->getNumberImages();
-        for ($i=0; $i<$nombrePage; $i++){
-            $im = new \Imagick();
-            $im->setResolution($x,$x);
-            $image = $file .'['. $i .']';
-            $im->readImage($image);
-            $im->writeImage("image".$i.".png");
-            $pdf->AddPage();
-            $pdf->Image("image".$i.".png");
-            try {
-                $qrCode = new \QrReader($im, \QrReader::SOURCE_TYPE_RESOURCE);
-            }catch (\Exception $e){
-                echo "non reconnu";
-                continue;
+        $range = range(0, $nombrePage-1);
+        $pdfQrCode = [];
+
+        while ($x <= 200 && !empty($range))
+        {
+            dump("Résolution : ". $x);
+            foreach($range as $i){
+                $nomFichier = "image".$i.".png";
+                $image = $file .'['. $i .']';
+                $imagick = $this->Imagick($image, $x, $nomFichier);
+                $qrcode = $this->QrCode($imagick);
+
+                if ($qrcode ){
+                    dump($qrcode);
+                    $pdfQrCode[$qrcode][$i] = $nomFichier;
+                    unset($range[$i]);
+                }
+                elseif($x == 150){
+                    $pdfQrCode['inconnu'][$i] = $nomFichier;
+                    unset($range[$i]);
+                }
             }
-            dump($qrCode->decode());
-            unlink("image".$i.".jpg");
-
+            $x +=5;
         }
-       # $pdf->Output($dir.'/test.pdf', "F");
 
-
+        $this->generePdfs($pdfQrCode,$dir);
 
         $output->writeln('Command result.');
+    }
+
+
+    public function Imagick($image, $x, $nomFichier)
+    {
+        $imagick = new \Imagick();
+        $imagick->setResolution($x,$x);
+        $imagick->readImage($image);
+        $imagick->writeImage($nomFichier);
+        return $imagick;
+    }
+
+    public function pdf($dir, $images, $qrcode)
+    {
+        $pdf = new \TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+        foreach ($images as $image) {
+            $pdf->AddPage();
+            $pdf->Image($image);
+            unlink($image);
+
+        }
+        $pdf->Output($dir.'/'. $qrcode . '.pdf', "F");
+    }
+
+    public function QrCode($im)
+    {
+        try {
+            $qrCode = new \QrReader($im, \QrReader::SOURCE_TYPE_RESOURCE);
+        }catch (\Exception $e){
+            echo "non reconnu";
+        }
+        return $qrCode->decode();
+    }
+
+    public function generePdfs($pdfQrcode, $dir){
+
+        foreach ($pdfQrcode as $qrcode => $page){
+            $this->pdf($dir, $page,$qrcode);
+
+        }
     }
 
 }
