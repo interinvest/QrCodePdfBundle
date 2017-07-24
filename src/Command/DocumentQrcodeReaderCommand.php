26008a3a24f70fb795cf159b5e5aaa7f48f7e289
yassine <ybellaaquoul@inter-invest.fr>
2017-07-24 11:59:25 +0200

    Update 11/07

--- src/Command/DocumentQrcodeReaderCommand.php
+++ src/Command/DocumentQrcodeReaderCommand.php
@@ -10,6 +10,13 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class DocumentQrcodeReaderCommand extends ContainerAwareCommand
 {
+
+    protected $dirName;
+
+    public function setDirName($dirName){
+        $this->dirName = $dirName;
+    }
+
     protected function configure()
     {
         $this
@@ -22,8 +29,18 @@ class DocumentQrcodeReaderCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $dir = $this->getContainer()->get('kernel')->getRootDir() . '/../../uploads/';
-        $file = $dir .  'NCA-20170705100943-yellow.pdf';
+        $dir =$this->getContainer()->getParameter('inputdir');
+        $destDir = new \DirectoryIterator($dir);
+        dump($destDir);
+        foreach ($destDir as $file)
+        {
+            if (!is_dir($file) && strpos($file->getBasename(),".pdf" ))
+            {
+                dump($file);
+
+dump($this->dirName = substr($file->getBasename(),0,3));
+        exit;
+        #$file = $dir .  'NCA-20170705100943-yellow.pdf';
         $im = new \Imagick();
 
         $im->readImage($file);
@@ -38,15 +55,15 @@ class DocumentQrcodeReaderCommand extends ContainerAwareCommand
             foreach($range as $i){
                 $nomFichier = "image".$i.".png";
                 $image = $file .'['. $i .']';
-                $imagick = $this->Imagick($image, $x, $nomFichier);
-                $qrcode = $this->QrCode($imagick);
+                $imagick = $this->imagick($image, $x, $nomFichier);
+                $qrcode = $this->qrCode($imagick);
 
                 if ($qrcode ){
                     dump($qrcode);
                     $pdfQrCode[$qrcode][$i] = $nomFichier;
                     unset($range[$i]);
                 }
-                elseif($x == 150){
+                elseif($x == 200){
                     $pdfQrCode['inconnu'][$i] = $nomFichier;
                     unset($range[$i]);
                 }
@@ -56,11 +73,14 @@ class DocumentQrcodeReaderCommand extends ContainerAwareCommand
 
         $this->generePdfs($pdfQrCode,$dir);
 
+
+            }
+        }
         $output->writeln('Command result.');
     }
 
 
-    public function Imagick($image, $x, $nomFichier)
+    public function imagick($image, $x, $nomFichier)
     {
         $imagick = new \Imagick();
         $imagick->setResolution($x,$x);
@@ -69,19 +89,7 @@ class DocumentQrcodeReaderCommand extends ContainerAwareCommand
         return $imagick;
     }
 
-    public function pdf($dir, $images, $qrcode)
-    {
-        $pdf = new \TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
-        foreach ($images as $image) {
-            $pdf->AddPage();
-            $pdf->Image($image);
-            unlink($image);
-
-        }
-        $pdf->Output($dir.'/'. $qrcode . '.pdf', "F");
-    }
-
-    public function QrCode($im)
+    public function qrCode($im)
     {
         try {
             $qrCode = new \QrReader($im, \QrReader::SOURCE_TYPE_RESOURCE);
@@ -97,6 +105,18 @@ class DocumentQrcodeReaderCommand extends ContainerAwareCommand
             $this->pdf($dir, $page,$qrcode);
 
         }
+    }
+
+    public function pdf($dir, $images, $qrcode)
+    {
+        $pdf = new \TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+        foreach ($images as $image) {
+            $pdf->AddPage();
+            $pdf->Image($image);
+            unlink($image);
+
+        }
+        $pdf->Output($dir.'/'. $this->dirName . '/' .$qrcode . '.pdf', "F");
     }
 
 }
